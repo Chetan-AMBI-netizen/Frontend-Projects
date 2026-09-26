83aad15cefaff1ab2151d7c2c30f08ad7b734741
Chetan-AMBI-netizen <chetanambiger0@gmail.com>
2026-09-26 20:30:56 +0530

    on this game the game was completed but only one resetbtn and newgame button function to add the logic that's it

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetbtn = document.querySelector("#reset-btn");
+let newgamebtn = document.querySelector ("#new-btn");
+let msgcontainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 let turn0 = true; 
 const winpatterns = [
     [0,1,2],
@@ -30,14 +33,25 @@ boxes.forEach((box) => {
 
     } );
 });
+const showwinner = (winner) => {
+    msg.innerText = `Conratulations, winner is ${winner}`;
+    msgcontainer.classList.remove("hide");
+
+
+};
+
  const checkwinner = () => {
     for( let pattern of winpatterns){
+
       let pos1val = boxes[pattern[0]].innerText;
       let pos2val = boxes[pattern[1]].innerText;
       let pos3val = boxes[pattern[2]].innerText;
       if(pos1val != "" && pos2val != "" && pos3val != ""){
         if(pos1val === pos2val && pos2val === pos3val){
-            console.log("winner");
+            console.log("winner",pos1val);
+            showwinner(pos1val);
+            
+
             
         }
 
